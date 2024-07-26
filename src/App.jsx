067da159e8d0b0337988d566a6eae8b862bc5511
Goitseone-Themba <goitseonerozthemba@gmail.com>
2024-07-27 00:25:23 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
-import { useState } from 'react'
-import { Dashboard } from './pages/Dashboard'
+import React from "react";
+import Signup from "./signup-login/Signup";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { useState } from "react";
+import { Dashboard } from "./pages/Dashboard";
+import { AuthProvider } from "./context/AuthContext";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
 
