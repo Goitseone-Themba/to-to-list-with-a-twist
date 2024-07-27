ae3cac979564beb4113ab27674aa2bfb78e02aa7
Goitseone-Themba <goitseonerozthemba@gmail.com>
2024-07-27 12:25:50 +0200

    abstract signup with firebase to AuthContext

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,31 +1,64 @@
-import { createContext, useState } from "react"
+import { createContext, useState } from "react";
+import {
+  auth,
+  createUserWithEmailAndPassword,
+  db,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "../services/firebase";
 
+export const AuthContext = createContext();
 
-export const AuthContext = createContext()
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
 
-export const AuthProvider = ({children}) => { 
-    const [user, setUser] = useState(null)
+  const signup = async (email, password) => {
+    console.log("signup attempt with ", email, password);
+    await createUserWithEmailAndPassword(auth, email, password)
+      .then((res) => {
+        setUser(res.user);
+        console.log(res)
+      })
+      .catch((err) => {
+        console.error(err);
+      });
 
-    const signup = async (email, password) => { 
-        console.log("signup attempt with ", email, password)
-     }
+  };
 
-     const login = (email, password) => { 
-        console.log("login attempt with ", email, password)
-      }
+  const login = async (email, password) => {
+    console.log("login attempt with ", email, password);
+    await signInWithEmailAndPassword(auth, email, password)
+      .then((res) => res)
+      .catch((err) => {
+        console.error(err);
+      });
 
-    const logout = async () => { 
-        console.log("logout attempt.")
-     }
+    return res;
+  };
 
-     const isAuthenticated = () => { 
-        console.log("isAuth from context")
-        return true
-      }
+  const logout = async () => {
+    console.log("logout attempt.");
+    await signOut(auth)
+      .then((res) => res)
+      .catch((err) => {
+        console.error(err);
+      });
 
-      return(
-        <AuthContext.Provider value={{user, signup, login, logout, isAuthenticated}}>
-            {children}
-        </AuthContext.Provider>
-      )
- }
+    return res;
+  };
+
+  const isAuthenticated = () => {
+    console.log("isAuth from context");
+    return true;
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ user, signup, login, logout, isAuthenticated }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
